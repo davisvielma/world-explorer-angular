b7342d70d25c45d1d4a9651f60fb4907e13d95ec
Davis Vielma <davisvielma@gmail.com>
2025-10-02 17:48:28 -0400

    Maquetando footer

--- src/app/country/layouts/contry-layout/country-layout.ts
+++ src/app/country/layouts/contry-layout/country-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Nabvar } from "@/shared/components/nabvar/nabvar";
+import { Footer } from "@/shared/components/footer/footer";
 
 @Component({
   selector: 'app-contry-layout',
-  imports: [RouterOutlet, Nabvar],
+  imports: [RouterOutlet, Nabvar, Footer],
   templateUrl: './country-layout.html',
 })
 export class CountryLayout {
